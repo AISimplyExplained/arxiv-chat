3e95952de8454b365092ca5e9a57426cee2efe29
vaibhav <vaibhavnayak018@gmail.com>
2024-07-04 22:06:31 +0530

    Drawer to sheet

--- components/PdfReader.tsx
+++ components/PdfReader.tsx
@@ -1,39 +1,36 @@
 'use client'
 import React from 'react'
 import {
-  Drawer,
-  DrawerContent,
-  DrawerTrigger,
-  DrawerClose
-
-} from '@/components/ui/drawer'
+  Sheet,
+  SheetContent,
+  SheetTrigger,
+  SheetClose
+} from '@/components/ui/sheet'
 import { Button } from './ui/button'
 import { Card } from './ui/card'
 import { X } from 'lucide-react'
 
 // @ts-ignore
-export default function PdfReader({pdfUrls}) {
+export default function PdfReader({ pdfUrls }) {
   return (
-    <Drawer>
-      <DrawerTrigger>
+    <Sheet>
+      <SheetTrigger>
         <Button className="absolute -top-12 left-2">Open PDF</Button>
-      </DrawerTrigger>
-      <DrawerContent className='bg-black h-[90%] p-2 rounded-lg max-w-5xl mx-auto'>
-          <div className="flex justify-end items-center p-2">
-            <DrawerClose>
-              <Button variant={"outline"}>Close</Button>
-            </DrawerClose>
-          </div>
-          <embed
-            src={pdfUrls?.url}
-            // @ts-ignore
-            allowFullScreen={true}
-            zoom
-            title="arXiv Paper"
-            width="100%"
-            height="100%"
-          ></embed>
-      </DrawerContent>
-    </Drawer>
+      </SheetTrigger>
+      <SheetContent
+        side={'bottom'}
+        className="bg-black h-[90%] p-2 rounded-lg max-w-5xl mx-auto"
+      >
+        <embed
+          src={pdfUrls?.url}
+          // @ts-ignore
+          allowFullScreen={true}
+          zoom
+          title="arXiv Paper"
+          width="100%"
+          height="100%"
+        ></embed>
+      </SheetContent>
+    </Sheet>
   )
 }
